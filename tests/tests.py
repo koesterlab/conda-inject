dd5b7d0ceef9ea056d064fc59423f8286dd79179
Johannes Köster <johannes.koester@uni-due.de>
2023-10-30 10:43:01 +0100

    chore: add test case for constraint injection (#8)

--- tests/tests.py
+++ tests/tests.py
@@ -16,3 +16,10 @@ def test_package_inject():
 def test_env_file_inject():
     with inject_env_file(Path("tests/test-env.yaml")):
         import humanfriendly  # noqa F401
+
+
+def test_env_inject_with_constraints():
+    env = {"channels": ["conda-forge"], "dependencies": ["humanfriendly =10.0"]}
+    with inject_env(env, with_constraints=["requests =2.31"]):
+        import humanfriendly  # noqa F401
+        import requests  # noqa F401
